cde1627aeb462c164b0204c38292e67123d6d0b4
Lasloruhberg <lasloruhberg@gmail.com>
2025-07-21 19:00:37 +0200

    fixed version number

--- astro_loader/__init__.py
+++ astro_loader/__init__.py
@@ -1,7 +1,7 @@
 from .normalisation.NormalisationMethod import NormalisationMethod
 from .image_loader import load_and_process_images, process_image
 
-__version__ = "1.1.0"
+__version__ = "0.1.0"
 
 __all__ = [
     "NormalisationMethod",
